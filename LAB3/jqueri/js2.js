1852e4322428122b6732ee3d0c34d3edb0d3d861
andriy2606 <32134515+andriy2606@users.noreply.github.com>
2018-12-05 19:50:47 +0200

    Add files via upload

---
+++ LAB3/jqueri/js2.js
@@ -0,0 +1,12 @@
+alert($("#elem").html());
+
+ $( document ).ready(function() {
+	 
+        let user = $('#list-header');
+    user.css('font-weight', 'bold');
+    user.append('16');
+
+var zamina = $("#name").html().replace('Peter','ANDRII');
+      $("#name").html(zamina);
+	 
+    });
